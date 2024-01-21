4ae3b8b15c8529306cbce5b719db46891927012c
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-21 17:46:48 +0530

    Added registration page!

---
+++ frontend/src/pages/Register/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '../../components/form/Form'
+const RegisterPage = () => {
+  return (
+    <>
+      <Form />
+    </>
+  )
+}
+
+export default RegisterPage
