6f4d6a6037ee3c8949479144230ec884d2596a8e
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-21 18:26:14 +0530

    Integrated dashboard route!

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate} from 'react-router-dom'
 import Register from '../pages/Register/RegisterPage'
-
+import Dashboard from '../pages/Dashboard/DashboardPage'
 const Router = () => {
     return (
         <Routes>
             <Route path='/' element={<Navigate to="/register" />}></Route>
-            <Route path='/register' element={<Register />}></Route>
+            <Route path='/register' element={<Register/>}></Route>
+            <Route path='/dashboard' element={<Dashboard/>}></Route>
         </Routes>
     )
 }
