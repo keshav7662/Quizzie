16c6338a3377874b3d0084ef9a2a287d8b4591c6
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-24 14:49:28 +0530

    feat : create-quiz service added!

--- frontend/src/services/QuizService.js
+++ frontend/src/services/QuizService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 const backendBaseURL = process.env.REACT_APP_BACKEND_BASE_URL;
+
 export const getAllQuizzes = async () => {
     try {
         const token = localStorage.getItem('token')
@@ -12,8 +13,19 @@ export const getAllQuizzes = async () => {
         return response.data
     } catch (error) {
         console.log(error);
+        toast.error(error.response.data.error, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
     }
 }
+
 export const deleteQuiz = async (id) => {
     try {
         const token = localStorage.getItem("token");
@@ -36,5 +48,52 @@ export const deleteQuiz = async (id) => {
         }
     } catch (error) {
         console.log(error);
+        toast.error(error.response.data.error, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
     }
 };
+
+export const createQuiz = async (newQuiz) => {
+    try {
+        const token = localStorage.getItem('token')
+        console.log(token)
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        };
+        const response = await axios.post(`${backendBaseURL}/quiz/create-quiz`, newQuiz, { headers })
+        if (response) {
+            toast.success(response.data.message, {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+        return response.data
+    } catch (error) {
+        console.log(error);
+        toast.error(error.response.data.error, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
+    }
+}
