2dcda8cd4b4298c6c0ebea9acfa07d54b45ae14e
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-24 10:55:11 +0530

    feat: Add routes for retrieving all quizzes and deleting a quiz!

--- backend/routes/quizRoute.js
+++ backend/routes/quizRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createQuestion, createQuiz } = require('../controllers/quizController');
+const { createQuestion, createQuiz, getAllQuizzes, deleteQuiz } = require('../controllers/quizController');
 const authenticatedUser = require('../middlewares/authentication')
+
 const router = express.Router();
 
-router.post('/create-quiz', authenticatedUser,createQuiz)
-router.post('/add-question/:id', authenticatedUser,createQuestion)
+router.post('/create-quiz', authenticatedUser, createQuiz)
+router.post('/add-question/:id', authenticatedUser, createQuestion)
+router.get('/all-quiz', authenticatedUser, getAllQuizzes)
+router.delete('/delete-quiz/:id', authenticatedUser, deleteQuiz)
 
 module.exports = router;
