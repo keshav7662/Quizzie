9d840bf5c9a19a7acac44aaefd15898494121a74
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-24 14:13:19 +0530

    feat : delete quiz service added!

--- frontend/src/services/QuizService.js
+++ frontend/src/services/QuizService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {toast} from 'react-toastify'
 const backendBaseURL = process.env.REACT_APP_BACKEND_BASE_URL;
 export const getAllQuizzes = async () => {
     try {
@@ -13,3 +14,27 @@ export const getAllQuizzes = async () => {
         console.log(error);
     }
 }
+export const deleteQuiz = async (id) => {
+    try {
+        const token = localStorage.getItem("token");
+        const headers = {
+            "Content-Type": "application/json",
+            Authorization: token,
+        };
+        const response = await axios.delete(`${backendBaseURL}/quiz/delete-quiz/${id}`, { headers });
+        if (response) {
+            toast.info(response.data.message, {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
