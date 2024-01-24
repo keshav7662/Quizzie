cae3d84a8d3ce9ba6ccb109fee4a12ca03e86731
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-24 14:47:02 +0530

    feat : Integrated create-quiz component!

--- frontend/src/pages/Dashboard/DashboardPage.js
+++ frontend/src/pages/Dashboard/DashboardPage.js
@@ -1,45 +1,59 @@
-import React, { useState } from "react";
-import Sidebar from "../../components/DashboardSidebar/Sidebar";
-import styles from "./dashboardPage.module.css";
-import QuizStatCard from "../../components/DashboardCards/quizStats/QuizStatCard";
-import TrendingStatCard from "../../components/DashboardCards/trendingStats/TrendingStatCard";
-import AnalysisPage from "../Analysis/AnalysisPage";
-import SweetAlert from "../../components/SweetAlert/SweetAlert";
+import React, { useState } from 'react';
+import Sidebar from '../../components/DashboardSidebar/Sidebar';
+import styles from './dashboardPage.module.css';
+import QuizStatCard from '../../components/DashboardCards/quizStats/QuizStatCard';
+import TrendingStatCard from '../../components/DashboardCards/trendingStats/TrendingStatCard';
+import AnalysisPage from '../Analysis/AnalysisPage';
+import SweetAlert from '../../components/SweetAlert/SweetAlert';
+import CreateQuizCard from '../../components/CreateQuizCard/CreateQuizCard';
+import AddQuiz from '../../components/AddQuiz';
+
 const DashboardPage = () => {
-  const [selectedButton, setSelectedButton] = useState("Dashboard");
+  const [selectedButton, setSelectedButton] = useState('Dashboard');
   const [showSweetAlert, setShowSweetAlert] = useState(false);
   const [confirmDelete, setConfirmDelete] = useState(false);
-
-  const handleButtonClick = (buttonName) => {
+  const [showCreateQuiz, setShowCreateQuiz] = useState(false);
+  const [showAddQuestion, setShowAddQuestion] = useState(false)
+  const handleSidebarButtonClick = (buttonName) => {
     setSelectedButton(buttonName);
   };
 
   const cancelDelete = () => {
     setShowSweetAlert(false);
   };
+
   const hideSweetAlert = () => {
     setShowSweetAlert(false);
   };
+
+  const hideCreateQuizCard = () => {
+    setShowCreateQuiz(false);
+    setShowAddQuestion(false)
+  };
+
   return (
     <div className={styles.dashboard}>
-      {showSweetAlert ? (
+      {showSweetAlert && (
         <div className={styles.sweetContainer} onClick={hideSweetAlert}>
-          <SweetAlert
-            setConfirmDelete={setConfirmDelete}
-            cancelDelete={cancelDelete}
-            setShowSweetAlert = {setShowSweetAlert}
-          />
+          <SweetAlert setConfirmDelete={setConfirmDelete} cancelDelete={cancelDelete} setShowSweetAlert={setShowSweetAlert} />
         </div>
-      ) : null}
+      )}
+
+      {showCreateQuiz ? (
+        <div className={styles.createQuizContainer} onClick={hideCreateQuizCard}>
+          <CreateQuizCard hideCreateQuizCard={hideCreateQuizCard} setShowAddQuestion={setShowAddQuestion} />
+        </div>
+      ) : showAddQuestion ? (<div className={styles.createQuizContainer} onClick={hideCreateQuizCard}>
+        <AddQuiz hideCreateQuizCard={hideCreateQuizCard} setShowAddQuestion={setShowAddQuestion} />
+      </div>) : null
+      }
       <div className={styles.sidebarContainer}>
-        <Sidebar
-          selectedButton={selectedButton}
-          handleButtonClick={handleButtonClick}
-        />
+        <Sidebar selectedButton={selectedButton} handleSidebarButtonClick={handleSidebarButtonClick} setShowCreateQuiz={setShowCreateQuiz} />
       </div>
+
       <div className={styles.quizStatsContainer}>
         <div className={styles.quizStatsSubContainer}>
-          {selectedButton === "Dashboard" ? (
+          {selectedButton === 'Dashboard' ? (
             <div className={styles.dashboardStats}>
               <QuizStatCard />
               <div className={styles.displayCards}>
@@ -49,17 +63,13 @@ const DashboardPage = () => {
                 </div>
               </div>
             </div>
-          ) : selectedButton === "Analytics" ? (
-            <AnalysisPage
-              setShowSweetAlert={setShowSweetAlert}
-              confirmDelete={confirmDelete}
-              setConfirmDelete={setConfirmDelete}
-            />
-          ) : null}
+          ) : (
+            <AnalysisPage setShowSweetAlert={setShowSweetAlert} confirmDelete={confirmDelete} setConfirmDelete={setConfirmDelete} />
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default DashboardPage;
+export default DashboardPage;
