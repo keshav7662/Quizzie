d6b6c684e4a6e9668dfb606de95addd7e8aea031
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-30 22:17:11 +0530

    timer values updated!

--- frontend/src/components/Timer/Timer.js
+++ frontend/src/components/Timer/Timer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './timer.module.css'
 const Timer = ({ addQuestionData, handleTimer }) => {
-    const Timer = ['OFF', '5 Sec', '10 Sec']
+    const Timer = ['OFF', '5', '10']
     const initialTimerState = 'OFF'
     return (
         <div className={styles.setTimerBox}>
             <span>Timer</span>
             {
                 Timer.map((time, index) => (
-                    <button type='button' className={`${addQuestionData.timer === time ? styles.selectedTimerStyle : addQuestionData.timer ? '' : time === initialTimerState ? styles.selectedTimerStyle : ''} ${styles.timerButtons}`} key={index} onClick={() => handleTimer(index, time)}>{time}</button>
+                    <button type='button' className={`${addQuestionData.timer === time ? styles.selectedTimerStyle : addQuestionData.timer ? '' : time === initialTimerState ? styles.selectedTimerStyle : ''} ${styles.timerButtons}`} key={index} onClick={() => handleTimer(index, time)}>{time !== 'OFF' ? `${time} sec` : 'OFF'}</button>
                 ))
             }
         </div>
