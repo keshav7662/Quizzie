558a2fa0def2323d44341541620afb12baf508f4
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-30 23:27:02 +0530

    added route to find quiz by id!

--- backend/routes/quizRoute.js
+++ backend/routes/quizRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { createQuestion, createQuiz, getAllQuizzes, deleteQuiz } = require('../controllers/quizController');
+const { createQuestion, createQuiz, getAllQuizzes, deleteQuiz, getQuizById } = require('../controllers/quizController');
 const authenticatedUser = require('../middlewares/authentication')
 
 const router = express.Router();
 
 router.post('/create-quiz', authenticatedUser, createQuiz)
 router.post('/add-question/:id?', authenticatedUser, createQuestion)
-router.get('/get-quiz/:quizId?', authenticatedUser, getAllQuizzes)
+router.get('/all-quiz', authenticatedUser, getAllQuizzes)
 router.delete('/delete-quiz/:id', authenticatedUser, deleteQuiz)
+router.get('/quiz-by-id/:id', authenticatedUser, getQuizById)
 
 module.exports = router;
