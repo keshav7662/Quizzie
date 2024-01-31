89349aca77eb53d71e9647134fa98a37cb02d405
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-31 23:35:58 +0530

    feat:custom hook useCountdownTimer!

---
+++ frontend/src/hooks/useCountdownTimer.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const useCountdownTimer = (handleTimerEnd) => {
+    const [availableSecond, setAvailableSecond] = useState(0);
+
+    useEffect(() => {
+        let interval;
+
+        if (availableSecond > 0) {
+            interval = setInterval(() => {
+                setAvailableSecond((prevSecond) => {
+                    const nextSecond = prevSecond - 1;
+                    if (nextSecond === 0 && handleTimerEnd) {
+                        clearInterval(interval);
+                        handleTimerEnd();
+                    }
+                    return nextSecond;
+                });
+            }, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [availableSecond, handleTimerEnd]);
+
+    const initialTimer = (seconds) => {
+        setAvailableSecond(seconds);
+    };
+
+    return { availableSecond, initialTimer };
+};
+
+export default useCountdownTimer;
