d21949d1432936f085081357081c1e6f9a35ee29
Keshav <keshavverma472@gmail.com>
2024-06-21 12:09:05 +0530

    some fixes

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: "https://quizzie-seven.vercel.app/",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"],
-    credentials: true,
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Welcome to QuizWhiz server');
